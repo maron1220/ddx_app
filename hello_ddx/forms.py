0227498a85e8c98e787a6bdd545f79533422b4ad
細川聖矢 <hosokawakiyoshiya@MacBook-Pro.local>
2021-06-03 00:21:12 +0900

    40%

--- hello_ddx/forms.py
+++ hello_ddx/forms.py
@@ -10,4 +10,13 @@ class DdxForm(forms.Form):
     ('ca','Ca')
   ]
 
-  bt_choice = forms.ChoiceField(label='radio',choices = bt_data,widget = forms.Select(attrs={'size':5}))
+  bt_choice = forms.ChoiceField(label='血液項目',choices = bt_data,widget = forms.Select(attrs={'size':5}),initial=['bun'])
+
+
+class UpdownForm(forms.Form):
+  status_data = [
+    ('up','上昇'),
+    ('down','低下'),
+  ]
+
+  status_choice = forms.ChoiceField(label='状態',choices = status_data,widget = forms.Select(attrs={'size':2}),initial=['up'])
--- hello_ddx/views.py
+++ hello_ddx/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic import TemplateView
-from .forms import DdxForm
+from .forms import DdxForm,UpdownForm
 from .models import Ddxlist,Firstddxlist,Secondddxlist,Thirdddxlist
+
 
 class BtDdxView(TemplateView):
 
@@ -13,6 +14,8 @@ class BtDdxView(TemplateView):
       'message':'ddx list',
       'form':DdxForm(),
       'ddxdata':ddxdata,
+      'secondform':UpdownForm(),
+     
     }
   
   def get(self,request):
@@ -20,10 +23,19 @@ class BtDdxView(TemplateView):
 
   def post(self,request):
     bt_ch = request.POST['bt_choice']
-    selected_bt = Ddxlist.objects.filter(bt_category = bt_ch)
+    status_ch = request.POST['status_choice']
+    status_message = ''
+    if status_ch == "up":
+      selected_bt = Ddxlist.objects.filter(bt_category = bt_ch).filter(true_up = True)
+      status_message = '上昇'
+    elif status_ch == "down":
+      selected_bt = Ddxlist.objects.filter(bt_category = bt_ch).filter(true_up = False)
+      status_message = '低下'
     self.params['ddxdata'] = selected_bt
-    self.params['result'] = bt_ch
+    self.params['result'] = '結果:('+bt_ch+status_message+')'
     self.params['form'] = DdxForm(request.POST)
+    self.params['secondform'] = UpdownForm(request.POST)
+  
     
     return render(request,'hello_ddx/index.html',self.params)
 
